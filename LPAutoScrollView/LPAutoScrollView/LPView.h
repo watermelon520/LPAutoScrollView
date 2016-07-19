c886cfa4176052121dc9bfc4b291965759a31c47
Luo ping <watermelon_lp@163.com>
2016-07-19 16:04:18 +0800

    初始化框架

---
+++ LPAutoScrollView/LPAutoScrollView/LPView.h
@@ -0,0 +1,15 @@
+//
+//  LPView.h
+//  LPScrollView
+//
+//  Created by Allen on 16/7/12.
+//  Copyright © 2016年 watermelon_lp. All rights reserved.
+//
+
+#import "LPContentView.h"
+
+@interface LPView : LPContentView
+
+@property (nonatomic, copy) NSString *title;
+
+@end
---
+++ LPAutoScrollView/LPAutoScrollView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LPAutoScrollView
+//
+//  Created by Allen on 16/7/19.
+//  Copyright © 2016年 watermelon_lp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
